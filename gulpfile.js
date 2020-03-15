7d582704a9c05469e5c6d7da73a206ff2e878cd0
Shuvo <aktarujjman@gmail.com>
2020-03-16 03:45:43 +0600

    before af

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
 
 function style(){
 	return(
 		gulp.src("src/scss/main.scss")
-		.pipe( sass({outputStyle:"expanded"}) ).on("error", sass.logError)
+		.pipe(sass()).on("error", sass.logError)	
+		.pipe(postcss([autoprefixer(), cssnano()]))
 		.pipe( gulp.dest("assets/css") )
 	);
 }
